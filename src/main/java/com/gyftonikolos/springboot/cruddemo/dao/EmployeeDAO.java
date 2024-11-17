0f83062a56d690bad8346f86afea5b9072d2d0bf
Gyftonikolos Nikolaos <64922119+Gyftonikolos@users.noreply.github.com>
2024-11-17 17:56:06 +0200

    Spring Boot Rest CRUD Employee Service find add update delete.

---
+++ src/main/java/com/gyftonikolos/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,26 @@
+package com.gyftonikolos.springboot.cruddemo.dao;
+
+import com.gyftonikolos.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
+
+
+
+
+
+
+
+
+
+
