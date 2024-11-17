b6a7fba69f2a162822163c795936a14966f593ff
Gyftonikolos Nikolaos <64922119+Gyftonikolos@users.noreply.github.com>
2024-11-17 14:04:09 +0200

    Spring Boot Rest CRUD Employee Refactored Service Layer.

---
+++ src/main/java/com/gyftonikolos/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,11 @@
+package com.gyftonikolos.springboot.cruddemo.dao;
+
+import com.gyftonikolos.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+}
